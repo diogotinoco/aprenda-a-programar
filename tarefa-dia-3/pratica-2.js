83b6886280a0be4cb217b1eaefccc4c537875ff7
Diogo Tinoco <diogotinoco@gmail.com>
2022-08-27 19:24:28 -0300

    ex2 dia3

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let contador = 10
+let n = 10
+let i = 1
+
+while (i <= n) {
+    let resultado = contador * i
+    let textodeexibicao = `${contador} X ${i} = ${resultado}` 
+    console.log(textodeexibicao)
+    i = i + 1
+}
